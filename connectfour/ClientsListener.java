03ad2c0a219bebc533dee5c3ec6b62b6131e2dff
hkai8 <115757055+hkai8@users.noreply.github.com>
2024-02-27 09:24:58 -0600

    Update ClientsListener.java

--- connectfour/ClientsListener.java
+++ connectfour/ClientsListener.java
@@ -25,10 +25,10 @@ public class ClientsListener implements Runnable
                 CommandFromServer cfs = (CommandFromServer)is.readObject();
 
                 // processes the received command
-                if(cfs.getCommand() == CommandFromServer.X_TURN)
-                    frame.setTurn('X');
-                else if(cfs.getCommand() == CommandFromServer.O_TURN)
-                    frame.setTurn('O');
+                if(cfs.getCommand() == CommandFromServer.BLACK_TURN)
+                    frame.setTurn("Black");
+                else if(cfs.getCommand() == CommandFromServer.RED_TURN)
+                    frame.setTurn("Black");
                 else if(cfs.getCommand() == cfs.MOVE)
                 {
                     String data = cfs.getData();
@@ -44,13 +44,13 @@ public class ClientsListener implements Runnable
                 {
                     frame.setText("Tie game.");
                 }
-                else if(cfs.getCommand() == CommandFromServer.X_WINS)
+                else if(cfs.getCommand() == CommandFromServer.BLACK_WINS)
                 {
-                    frame.setText("X wins!");
+                    frame.setText("Black wins!");
                 }
-                else if(cfs.getCommand() == CommandFromServer.O_WINS)
+                else if(cfs.getCommand() == CommandFromServer.RED_WINS)
                 {
-                    frame.setText("O wins!");
+                    frame.setText("Red wins!");
                 }
             }
         }
