a0ecb8332f6339c304e9438d9d92a4793f74d3aa
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2024-02-28 19:23:57 -0600

    fixed command

--- connectfour/CommandFromClient.java
+++ connectfour/CommandFromClient.java
@@ -2,14 +2,16 @@ import java.io.Serializable;
 
 public class CommandFromClient implements Serializable
 {
+    public static final int MOVE    =0;
+    public static final int RESTART =1;
+    public static final int CLOSING = 2;
+    public static final int CONFIRM = 3;
     // The command being sent to the server
     private int command;
     // Text data for the command
     private String data ="";
 
-    // Command list
-    public static final int MOVE    =0;
-    public static final int RESTART =1;
+
 
     public CommandFromClient(int command, String data) {
         this.command = command;
--- connectfour/TTTFrame.java
+++ connectfour/TTTFrame.java
@@ -1,42 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Arrays;
 import java.util.Collections;
+import java.awt.*;
+import java.util.*;
 
-public class TTTFrame extends JFrame implements WindowListener, MouseListener {
+
+public class TTTFrame extends JFrame implements  WindowListener, MouseListener {
     // Display message
     private String text = "";
     // the letter you are playing as
     private char player;
     // stores all the game data
     private GameData gameData = null;
-    // output stream to the server
+    private long startTime = -1;
+    private boolean sentResetRequest = false;
+    private boolean sendingConfirmReset = false;
+    // output stream to the s
     ObjectOutputStream os;
+
+    private char turn;
 
     public TTTFrame(GameData gameData, ObjectOutputStream os, char player)
     {
-        super("Connect 4 Game");
-        // sets the attributes
+        super("TTT Game");
         this.gameData = gameData;
         this.os = os;
         this.player = player;
 
-        // adds a KeyListener to the Frame
+        addWindowListener((WindowListener) this);
         addMouseListener(this);
 
         // makes closing the frame close the program
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         // Set initial frame message
-        if(player == 'B')
-            text = "Waiting for Red to Connect";
-        else text = "Waiting for Black to Connect";
+        if(player == 'X')
+            text = "Waiting for Black to Connect";
 
-        setSize(760,750);
+        setSize(700,800);
         setResizable(false);
         setAlwaysOnTop(true);
         setVisible(true);
@@ -45,95 +51,43 @@ public class TTTFrame extends JFrame implements WindowListener, MouseListener {
     public void paint(Graphics g)
     {
         // draws the background
-        g.setColor(Color.YELLOW);
+        g.setColor(Color.BLUE);
         g.fillRect(0,0,getWidth(),getHeight());
 
         // draws the display text to the screen
-        g.setColor(Color.RED);
-        g.setFont(new Font("Times New Roman",Font.BOLD,30));
-        g.drawString(text,20,55);
 
         // draws the tic-tac-toe grid lines to the screen
+        /*g.setColor(Color.RED);
+        for(int y =0;y<=1; y++)
+            g.drawLine(0,(y+1)*133+60,getWidth(),(y+1)*133+60);
+        for(int x =0;x<=1; x++)
+            g.drawLine((x+1)*133,60,(x+1)*133,getHeight());*/
 
-        for(int i =0;i<6;i++){
-            for(int j =0;j<7;j++){
-                if(gameData.getGrid()[i][j]==' '){
-                    g.setColor(Color.WHITE);
-                    g.fillOval(100*j+35, 100*i+110, 90 , 90);
-                }
-                else if(gameData.getGrid()[i][j]=='R'){
-                    g.setColor(Color.RED);
-                    g.fillOval(100*j+35, 100*i+110, 90 , 90);
-                }
-                else{
-                    g.setColor(Color.BLACK);
-                    g.fillOval(100*j+35, 100*i+110, 90 , 90);
-                }
-            }
-        }
-        if(checkWin('R')){
-
-        }
-        else if(checkWin('B')){
-
-    }
-        //change oval
+        g.setColor(Color.white);
 
         // draws the player moves to the screen
-    /*    g.setFont(new Font("Times New Roman",Font.BOLD,70));
-        for(int r=0; r<gameData.getGrid().length; r++)
-            for(int c=0; c<gameData.getGrid().length; c++)
-                g.drawString(""+gameData.getGrid()[r][c],c*133+42,r*133+150);
+        int actualR = 0;
+        for(int r=gameData.getGrid().length-1; r>=0; r--) {
+            for(int c=0; c<gameData.getGrid()[0].length; c++) {
+                if(gameData.getGrid()[r][c]=='X') {
+                    g.setColor(Color.RED);
+                } else if(gameData.getGrid()[r][c]=='O') {
+                    g.setColor(Color.BLACK);
+                } else {
+                    g.setColor(Color.white);
+                }
+                g.fillOval(c*100+50,actualR*100+50, 50,50);
 
-     */
+            }
+            actualR++;
+        }
+
+        //g.drawString(""+gameData.getGrid()[r][c],c*133+42,r*133+150);
+        g.setColor(Color.RED);
+        g.setFont(new Font("Times New Roman",Font.BOLD,30));
+        g.drawString(text,20,750);
+
     }
-
-    public boolean checkWin(char player) {
-        char[][] grid = gameData.getGrid();
-        // Check horizontal
-        for (int row = 0; row < 6; row++) {
-            for (int col = 0; col < 4; col++) {
-                if (grid[row][col] == player &&
-                        grid[row][col + 1] == player &&
-                        grid[row][col + 2] == player &&
-                        grid[row][col + 3] == player) {
-                    return true;
-                }
-            }
-        }
-        for (int col = 0; col < 7; col++) {
-            for (int row = 0; row < 3; row++) {
-                if (grid[row][col] == player &&
-                        grid[row + 1][col] == player &&
-                        grid[row + 2][col] == player &&
-                        grid[row + 3][col] == player) {
-                    return true;
-                }
-            }
-        }
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 4; col++) {
-                if (grid[row][col] == player &&
-                        grid[row + 1][col + 1] == player &&
-                        grid[row + 2][col + 2] == player &&
-                        grid[row + 3][col + 3] == player) {
-                    return true;
-                }
-            }
-        }
-        for (int row = 3; row < 6; row++) {
-            for (int col = 0; col < 4; col++) {
-                if (grid[row][col] == player &&
-                        grid[row - 1][col + 1] == player &&
-                        grid[row - 2][col + 2] == player &&
-                        grid[row - 3][col + 3] == player) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
 
     public void setText(String text) {
         this.text = text;
@@ -141,12 +95,22 @@ public class TTTFrame extends JFrame implements WindowListener, MouseListener {
     }
 
 
-    public void setTurn(char turn) {
-        if(turn==player)
+    public void setTurn(char t) {
+        turn = t;
+
+        if(t==player)
             text = "Your turn";
         else
         {
-            text = turn+"'s turn.";
+            if(t=='e') {
+
+            } else {
+                if(t=='X') {
+                    text = "Red's turn";
+                } else {
+                    text = "Black's turn";
+                }
+            }
         }
         repaint();
     }
@@ -157,6 +121,98 @@ public class TTTFrame extends JFrame implements WindowListener, MouseListener {
         repaint();
     }
 
+
+
+
+
+    public void resetGrid() {
+
+        gameData.reset();
+        startTime=-1;
+        sentResetRequest=false;
+        sendingConfirmReset=false;
+    }
+    public String getGrid() {return Arrays.deepToString(gameData.getGrid());}
+
+
+    @Override
+    public void windowOpened(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        try {
+            System.out.println("closing window");
+            os.writeObject(new CommandFromClient(CommandFromClient.CLOSING, ""+player));
+        } catch (IOException ex) {
+            System.out.println("Exception with closing window");
+            ex.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+
+    }
+
+    public void closing() throws InterruptedException {
+        System.out.println("Inside closing method");
+        startTime = System.currentTimeMillis();
+        long prev = -1;
+        while(true) {
+            long timepassed=System.currentTimeMillis()-startTime;
+            long secondspassed=timepassed/1000;
+            if(secondspassed>prev) {
+                text = "Other Client Disconnected. Closing in: "+(5-secondspassed);
+                System.out.println(text);
+                prev=secondspassed;
+                repaint();
+            }
+            if(secondspassed>=6) {
+                break;
+            }
+
+        }
+        System.exit(0);
+
+    }
+    public void initializeConfirm (String m, String title)
+    {
+        sendingConfirmReset=true;
+        text = "Other client wants to reset. Press R to reset";
+
+
+
+
+
+
+        //JOptionPane.showMessageDialog(null, m, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+    public boolean getSentResetRequest() {
+        return sentResetRequest;
+    }
 
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -193,40 +249,6 @@ public class TTTFrame extends JFrame implements WindowListener, MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {
-
-    }
-    @Override
-    public void windowOpened(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowClosing(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowIconified(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowActivated(WindowEvent e) {
-
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) {
 
     }
 }
