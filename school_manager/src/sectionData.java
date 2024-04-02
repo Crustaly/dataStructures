3c5e62287fa1f83c4149e007b84402883b7b3ede
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2024-04-02 09:58:00 -0500

    sec data

--- school_manager/src/sectionData.java
+++ school_manager/src/sectionData.java
@@ -1,2 +1,37 @@
+import java.sql.*;
+
 public class sectionData {
+    int id, courseId, teacherId;
+    Statement sn;
+
+    public sectionData(int id, int courseId, int teacherId, Statement sn) throws SQLException {
+        this.id = id;
+        this.courseId = courseId;
+        this.teacherId = teacherId;
+        this.sn = sn;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
 }
