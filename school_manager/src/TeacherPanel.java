a61089d89e84bc026db21ea3b05a793a99143653
hkai8 <huangkailin8@gmail.com>
2024-04-03 09:08:33 -0500

    dfd

--- school_manager/src/TeacherPanel.java
+++ school_manager/src/TeacherPanel.java
@@ -1,2 +1,26 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class TeacherPanel {
+    JLabel firstName = new JLabel("First Name: ");
+    JLabel lastName = new JLabel("Last Name: ");
+    JLabel ID = new JLabel("ID: ");
+    JTextField first = new JTextField();
+    JTextField last = new JTextField();
+    JTextField IDs = new JTextField();
+    JButton save = new JButton("Save");
+    JButton clear = new JButton("Clear");
+    JButton saveChanges = new JButton("Save changes");
+    JButton deleteContact = new JButton("Delete contact");
+
+    ArrayList<Data> people = new ArrayList<>();
+    JList<Data> myContacts = new JList<>();
+
+    JScrollPane pane = new JScrollPane();
+
+    ArrayList<sectionData> sections = new ArrayList<>();
+    JList<sectionData> jSections = new JList<>();
+    JScrollPane sScrolling = new JScrollPane();
+
+
 }
