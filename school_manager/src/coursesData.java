db82c982f70c1c0ea124662bb4167a251d42f681
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2024-04-03 07:44:31 -0500

    made a small course stuff
    also added comparator to compare and sort them later!
    
    lmk if its bad or if im missing any part

---
+++ school_manager/src/coursesData.java
@@ -0,0 +1,48 @@
+public class coursesData
+{
+    String ID;
+    String courseName;
+    int type;
+    public coursesData(String ID, String courseName, int type)
+    {
+        this.ID = ID;
+        this.courseName = courseName;
+        this.type = type;
+    }
+
+    public void setCourseName(String a)
+    {
+        courseName = a;
+    }
+    public void setType(int a)
+    {
+        type = a;
+    }
+    public String getCourseName()
+    {
+        return courseName;
+    }
+    public int getType()
+    {
+        return type;
+    }
+    public String getID()
+    {
+        return ID;
+    }
+    public void setID(String ID) {this.ID = ID;}
+    @Override
+    public String toString() {
+        //return courseName;
+        return "ID: " + ID + " " + courseName;
+    }
+    public boolean compareCourses(coursesData course) {
+        if(course.getType()==getType()&&course.getID()==getID()&&course.getCourseName()==getCourseName()) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
