10aaf0394d525f27591e43de00fc2c09f8ccdb0d
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2024-04-04 09:19:42 -0500

    adding some bounds to student panel

--- school_manager/src/StudentPanel.java
+++ school_manager/src/StudentPanel.java
@@ -14,7 +14,7 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.Random;
 import java.sql.*;
-public class StudentPanel {
+public class StudentPanel extends JPanel{
     JLabel firstName = new JLabel("First Name:");
     JLabel lastName = new JLabel("Last Name:");
     JLabel ID = new JLabel("ID:");
@@ -42,6 +42,50 @@ public class StudentPanel {
 
     ArrayList<Component> allComponents = new ArrayList<>();
     JScrollPane bar;
+
+    public StudentPanel(int width, int height, Statement statementName) throws SQLException {
+        setSize(width, height);
+        setLayout(null);
+        //setBounds(0,0,w,h);
+        sections = new ArrayList<>();
+        jSections = new JList<>();
+        sectionScrolling = new JScrollPane();
+        schedule.setBounds(50,420,100,10);
+        add(schedule);
+
+        //missing the try part add files in!
+
+
+        firstName.setText("first");
+        firstName.setBounds(280, 100, 100, 50);
+        add(firstName);
+
+        first.setText("first");
+        first.setBounds(390,120,100,20);
+        add(first);
+
+        setVisible(true);
+
+        lastName.setBounds(280, 150, 100, 20);
+        add(lastName);
+        last.setText("last");//get from sql
+        last.setBounds(390, 150, 100, 20);
+        add(last);
+
+        ID.setBounds(280, 200, 100, 20);
+        add(ID);
+        IDs.setText("ID");//get from sql
+        IDs.setBounds(390, 200, 100, 20);
+        IDs.setEditable(false);
+        add(IDs);
+
+        saveChanges.setBounds(280, 310, 100, 20);
+        saveChanges.setText("Save Changes");
+        saveChanges.setVisible(true);
+        add(saveChanges);
+        setVisible(true);
+
+    }
 
 
 }
