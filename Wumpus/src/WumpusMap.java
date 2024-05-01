247e1d30108e731a90ed78ec588857b0bf1ed0b0
hkai8 <huangkailin8@gmail.com>
2024-05-01 03:22:25 -0500

    square

--- Wumpus/src/WumpusMap.java
+++ Wumpus/src/WumpusMap.java
@@ -1,2 +1,90 @@
 public class WumpusMap {
+
+    public static final int NUM_ROWS = 10;
+    public static final int NUM_COLUMNS = 10;
+    public static final int NUM_PITS = 10;
+
+    private WumpusSquare[][] grid;
+    public static int ladderC, ladderR;
+
+    public WumpusMap(){ createMap();}
+
+    public void createMap(){
+        grid = new WumpusSquare[10][10];
+
+        for(int i = 0; i<grid.length;i++){
+            for(int j = 0; j<grid[0].length; j++){
+                grid[i][j] = new WumpusSquare();
+            }
+        }
+
+        for(int i = 0; i<NUM_PITS; i++){
+            while(true){
+                int r = (int) (Math.random() * 8) + 1;
+                int c = (int) (Math.random() * 8) + 1;
+                if(!grid[r][c].getPit() && !grid[r][c].getBreeze()){
+                    grid[r][c].setPit(true);
+                    grid[r-1][c].setBreeze(true);
+                    grid[r+1][c].setBreeze(true);
+                    grid[r][c-1].setBreeze(true);
+                    grid[r][c+1].setBreeze(true);
+                    break;
+                }
+            }
+
+            while(true) {
+                int r = (int) (Math.random() * 10);
+                int c = (int) (Math.random() * 10);
+                if(!grid[r][c].getPit()) {
+                    grid[r][c].setGold(true);
+                    break;
+                }
+            }
+
+            while(true) {
+                int r = (int) (Math.random() * 8)+1;
+                int c = (int) (Math.random() * 8)+1;
+                if(!grid[r][c].getPit() && !grid[r][c].getGold()) {
+                    grid[r][c].setWumpus(true);
+                    grid[r-1][c].setStench(true);
+                    grid[r+1][c].setStench(true);
+                    grid[r][c-1].setStench(true);
+                    grid[r][c+1].setStench(true);
+                    break;
+                }
+            }
+
+            while(true) {
+                int r = (int) (Math.random() * 10);
+                int c = (int) (Math.random() * 10);
+                if(!grid[r][c].getGold()&&!grid[r][c].getWumpus() && !grid[r][c].getPit()) {
+                    grid[r][c].setLadder(true);
+                    ladderC = c;
+                    ladderR = r;
+                    break;
+                }
+            }
+        }
+    }
+
+    public WumpusSquare getSquare(int row, int col) {
+        if(col >= grid[0].length || col < 0 || row < 0 || row >= grid.length) {
+            return null;
+        }
+        return grid[row][col];
+        //return getSquare(row, col);
+    }
+
+    public String toString() {
+        String str = "";
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                str+=grid[i][j].toString();
+            }
+            if(i != grid.length-1) {
+                str+="\n";
+            }
+        }
+        return str;
+    }
 }
--- Wumpus/src/WumpusSquare.java
+++ Wumpus/src/WumpusSquare.java
@@ -1,2 +1,35 @@
 public class WumpusSquare {
+    private boolean gold, ladder, pit, breeze, wumpus, deadWumpus, stench, visited;
+
+    public WumpusSquare(){
+        gold = ladder = pit = breeze = wumpus = deadWumpus = stench = visited = false;
+    }
+
+    public boolean getGold() {return gold;}
+    public boolean getLadder() {return ladder;}
+    public boolean getPit() {return pit;}
+    public boolean getBreeze() {return breeze;}
+    public boolean getWumpus() {return wumpus;}
+    public boolean getDeadWumpus() {return deadWumpus;}
+    public boolean getStench() {return stench;}
+    public boolean getVisited() {return visited;}
+
+    public void setGold(boolean b) {gold = b;}
+    public void setLadder(boolean b) {ladder = b;}
+    public void setPit(boolean b) {pit = b;}
+    public void setBreeze(boolean b) {breeze = b;}
+    public void setWumpus(boolean b) {wumpus= b;}
+    public void setDeadWumpus(boolean b) {deadWumpus = b;}
+    public void setStench(boolean b) {stench = b;}
+    public void setVisited(boolean b) {visited = b;}
+
+    @Override
+    public String toString() {
+        if(wumpus) {return "W";}
+        if(deadWumpus) {return "D";}
+        if(ladder) {return "L";}
+        if(pit) {return "P";}
+        if(gold) {return "G";}
+        return "*";
+    }
 }
