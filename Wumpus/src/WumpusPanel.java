084267ec02fd3842793a2efff778c5e5b36fd7e3
Crystal Yang <106780309+Crustaly@users.noreply.github.com>
2024-05-05 13:45:07 -0500

    d

--- Wumpus/src/WumpusPanel.java
+++ Wumpus/src/WumpusPanel.java
@@ -12,7 +12,7 @@ public class WumpusPanel extends JPanel implements KeyListener {
 
     private boolean cheat=false;
     private WumpusPlayer player;
-    private BufferedImage fog, gold, ladder, floor, arrow, pit, breeze, stench,wumpus, deadWumpus,  playerUp, playerDown, playerLeft, playerRight;
+    private BufferedImage  gold, fog, ladder, floor, arrow, pit, breeze, stench,wumpus, deadWumpus,  playerUp, playerDown, playerLeft, playerRight;
     static ArrayList<String> list;
     private WumpusMap map;
     private BufferedImage buffer;
