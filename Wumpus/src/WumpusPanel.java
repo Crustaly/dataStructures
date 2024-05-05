dd205d36a7b67e2241dcbc404fa0aa6c2a12997a
hkai8 <huangkailin8@gmail.com>
2024-05-05 14:34:20 -0500

    fixed

--- Wumpus/src/WumpusPanel.java
+++ Wumpus/src/WumpusPanel.java
@@ -296,10 +296,10 @@ public class WumpusPanel extends JPanel implements KeyListener {
         player = new WumpusPlayer();
         status = PLAYING;
         map = new WumpusMap();
+        player.setColPosition(map.getLadderCol());
+        player.setRowPosition(map.getLadderRow());
         WumpusSquare sq = map.getSquare(player.getRowPosition(), player.getColPosition());
         sq.setVisited(true);
-        player.setColPosition(map.getLadderCol());
-        player.setRowPosition(map.getLadderCol());
         msgs = new ArrayList<>();
         cheatMode = false;
     }
